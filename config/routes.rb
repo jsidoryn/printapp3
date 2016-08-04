22cf7a085c0ca08c5135180689947189789a4e27
Jason Sidoryn <jason@katalyst.com.au>
2016-08-04 10:54:31 +0930

    added in basic views and links for signin / signout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :jobs, only: [:index]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	root to: "organisations#index"
+	root to: "jobs#index"
 end
